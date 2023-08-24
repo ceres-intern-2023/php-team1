a78a152782559565708b9d896a4b82123b5a809b
wakki0202 <ytk0202@docomo.ne.jp>
2023-08-24 16:22:01 +0900

    動物画像追加 登録画面修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,18 +29,18 @@
             </div>
 
             <!-- 年齢 -->
-            <div class="mt-4">
+            <!-- <div class="mt-4">
                 <x-label for="years_old" :value="__('年齢')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+            </div> -->
 
             <!-- 性別 -->
-            <div class="mt-4">
+            <!-- <div class="mt-4">
                 <x-label for="gender" :value="__('性別')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+            </div> -->
 
             <!-- Password -->
             <div class="mt-4">
