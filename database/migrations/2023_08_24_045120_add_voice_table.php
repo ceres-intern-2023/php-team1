60ff12a25ee4a398f61677432771c244987d2e27
aoba-s <s1f102100614@iniad.org>
2023-08-24 14:26:06 +0900

    usersテーブル、voicesテーブルの修正・作成

---
+++ database/migrations/2023_08_24_045120_add_voice_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddVoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voices', function (Blueprint $table) {
+            $table->id();
+            $table->string('youtube_url');  // YoutubeURL
+            $table->foreignId('user_id')->constrained('users'); // usersのid
+            $table->foreignId('animal_id')->constrained('animals'); // animalsのid
+            $table->integer('like');  // いいね数
+            $table->softDeletes();  // ソフトデリート
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voices');
+    }
+}
